5ca9f9fa6d4e37146c2d064cd1277d0c0c445732
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-01-21 16:14:01 +0200

    setting up app

---
+++ tenLibrariesApp/tenLibrariesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  tenLibrariesAppApp.swift
+//  tenLibrariesApp
+//
+//  Created by asmaa gamal  on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct tenLibrariesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
